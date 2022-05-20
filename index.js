3644b57e42d63ac5b5e38f92ed0ccacbf898b7ad
iQiaFUN <github@iqia.fun>
2022-05-20 14:07:55 +0800

    update

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const ProgressBar = require('progress');
 const path = require('path');
+const tmpcfg = JSON.parse(NIL.IO.readFrom(path.join(__dirname, 'example.json')));
+
+function checkFile(file, text) {
+    if (NIL.IO.exists(path.join(__dirname, file)) == false) {
+        NIL.IO.WriteTo(path.join(__dirname, file), text);
+    }
+}
+
+checkFile("config.json", tmpcfg);
 const cfg = JSON.parse(NIL.IO.readFrom(path.join(__dirname, 'config.json')));
 const vcfg = NIL._vanilla.cfg;
 const backup_path = path.join(__dirname,cfg.path);
